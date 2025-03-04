c466775ea33aeff07f3710e7b8c4e37be3c85311
Vikas <vikas.kumar.singh1198@gmail.com>
2025-03-04 19:35:10 +0530

    implemented signin route

--- backend/routes/user.js
+++ backend/routes/user.js
@@ -4,15 +4,21 @@ const router = express.Router();
 const { User } = require("../db/db");
 const bcrypt = require("bcrypt");
 const zod = require("zod");
-const JWT_SEC = process.env.JWT_SECRET
+const JWT_SECRET = process.env.JWT_SECRET
+const jwt = require("jsonwebtoken")
 
 
 const inputSchema = zod.object({
-  username: zod.string().max(50),
-  firstName: zod.string().max(12),
-  lastName: zod.string().max(12),
-  password: zod.string().max(12),
+  username: zod.string().email(),
+  firstName: zod.string(),
+  lastName: zod.string(),
+  password: zod.string(),
 });
+
+const loginSchema = zod.object({
+    username: zod.string().email(),
+    password: zod.string(),
+})
 
 router.post("/signup", async function (req, res) {
   const { username, password, firstName, lastName } = req.body;
@@ -58,6 +64,51 @@ router.post("/signup", async function (req, res) {
   }
 });
 
-router.post("/signin")
+router.post("/signin" , async function (req,res) {
+    const {username,password} = req.body;
+
+    try{
+        const inputValidation = loginSchema.safeParse({
+            username,
+            password
+        });
+    
+        if(!inputValidation.success){
+            return res.status(401).json({
+                message:"Invalid Inputs"
+            });
+        }
+    
+        const user = await User.findOne({
+            username
+        });
+    
+        if(!user){
+            return res.status(400).json({
+                message:"User not found"
+            })
+        }
+
+        const isMatch =  bcrypt.compare(password,user.password);
+
+        if(!isMatch){
+            return res.status(401).json({
+                message:"Invalid Credentials"
+            });
+        }
+
+        const token =  jwt.sign({userId:user._id,},JWT_SECRET);
+
+        res.json({
+            message:"User logged in Succesfully",
+            token,
+        });
+
+    }catch(error){
+        console.log(error)
+        res.status(500).json({ message: "Internal Server Error" });
+    }
+    
+})
 
 module.exports = router;
